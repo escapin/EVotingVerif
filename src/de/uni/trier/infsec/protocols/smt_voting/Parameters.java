c1a2ff3bfd790fe783c176a689f3e5ffcb715426
Tomasz Truderung <ttruderung@gmail.com>
2013-02-11 10:31:43 +0100

    unsessential changes in Parameters.java

--- src/de/uni/trier/infsec/protocols/smt_voting/Parameters.java
+++ src/de/uni/trier/infsec/protocols/smt_voting/Parameters.java
@@ -1,12 +1,12 @@
 package de.uni.trier.infsec.protocols.smt_voting;
 
 public class Parameters {
-	static final int DEFAULT_LISTEN_PORT_SERVER_AMT = 88; // Listen port for Voter requests
-	static final int DEFAULT_LISTEN_PORT_SERVER_SMT = 89; // Listen port for Voter requests
+	static final int DEFAULT_LISTEN_PORT_SERVER_AMT = 4088; // Listen port for Voter requests
+	static final int DEFAULT_LISTEN_PORT_SERVER_SMT = 4089; // Listen port for Voter requests
 
-	static final int DEFAULT_LISTEN_PORT_BBOARD_AMT = 90; 		// Listen port for Server requests
-	static final int DEFAULT_LISTEN_PORT_BBOARD_SMT = 91; 		// Listen port for Server requests
-	static final int DEFAULT_LISTEN_PORT_BBOARD_REQUEST = 92; 	// Listen port for result requests
+	static final int DEFAULT_LISTEN_PORT_BBOARD_AMT = 4090; 		// Listen port for Server requests
+	static final int DEFAULT_LISTEN_PORT_BBOARD_SMT = 4091; 		// Listen port for Server requests
+	static final int DEFAULT_LISTEN_PORT_BBOARD_REQUEST = 4092; 	// Listen port for result requests
 
 	static final String DEFAULT_HOST_SERVER = "localhost";
 	static final String DEFAULT_HOST_BBOARD = "localhost";
