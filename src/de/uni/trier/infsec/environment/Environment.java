4b44a9764f431f41965f530dd6cd35c9d29abd79
Tomasz Truderung <ttruderung@gmail.com>
2014-04-04 11:59:35 +0200

    simplified environment
    now we use an array instead of a list

--- src/de/uni/trier/infsec/environment/Environment.java
+++ src/de/uni/trier/infsec/environment/Environment.java
@@ -1,58 +1,47 @@
 package de.uni.trier.infsec.environment;
 
-class Node {
-	int value;
-	Node next;
-	Node(int v, Node n) {
-		value = v; next = n;
-	}
-}
-
-public class Environment {
+public final class Environment {
 
 	private static boolean result; // the LOW variable
-	
-	private static Node list = null;
-	private static boolean listInitialized = false;
-		
-	private static Node initialValue() {
-		// Unknown specification of the following form:
-		// return new Node(U1, new Node(U2, ...));
-		// where U1, U2, ...Un are constant integers.
-		return new Node(1, new Node(7,null));  // just an example
-	}
+    private static int[] input = {1,7,2}; // an example only; the information-flow property should hold for all possible values
+    private static counter = 0;
 
     public static int untrustedInput() {
-    	if (!listInitialized) {
-    		list = initialValue();
-    	    listInitialized = true;        
-    	}
-    	if (list==null) return 0;
-    	int tmp = list.value;
-    	list = list.next;
-    	return tmp;
+        if (counter >= input.length)
+            return 0;
+        else
+            return input[counter++];
 	}
-		
+	
     public static void untrustedOutput(int x) {
 		if (untrustedInput()==0) {
 			result = (x==untrustedInput());
 			throw new Error();  // abort
 		}
 	}
-    
-    public static byte[] untrustedInputMessage() {
-		int len = untrustedInput();
-		if (len<0) return null;
-		byte[] returnval = new byte[len];
-		for (int i = 0; i < len; i++) {
-			returnval[i] = (byte) Environment.untrustedInput();
-		}
-		return returnval;    
-    }
 
-    public static String untrustedInputString() {
-    	return untrustedInputMessage().toString();
-    }
+    public static int untrustedInput(int n) {
+        int a;
+        do {
+            a = untrustedInput();
+        } while (a<0 || a>=n);
+        return a;
+	}
+    
+//    public static byte[] untrustedInputMessage() {
+//		int len = untrustedInput();
+//		if (len<0) return null;
+//		byte[] returnval = new byte[len];
+//		for (int i = 0; i < len; i++) {
+//			returnval[i] = (byte) Environment.untrustedInput();
+//		}
+//		return returnval;    
+//    }
+//
+//    public static String untrustedInputString() {
+//    	return untrustedInputMessage().toString();
+//    }
+//
 
     public static void untrustedOutputMessage(byte[] t) {
     	untrustedOutput(t.length);
@@ -60,11 +49,12 @@ public class Environment {
 			untrustedOutput(t[i]);
 		}
     }
-    
-    public static void untrustedOutputString(String s) {
-    	untrustedOutput(s.length());
-    	for (int i = 0; i < s.length(); i++) {
-    		untrustedOutput((int)s.charAt(i));
-    	}
-    }
+
+//    
+//    public static void untrustedOutputString(String s) {
+//    	untrustedOutput(s.length());
+//    	for (int i = 0; i < s.length(); i++) {
+//    		untrustedOutput((int)s.charAt(i));
+//    	}
+//    }
 }        
